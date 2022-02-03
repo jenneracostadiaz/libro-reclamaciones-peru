e329698dd89c416afa459a7f3aef72fc145f7f26
markethinkers <projects@markethinkers.pe>
2022-02-03 10:52:27 -0500

    Completando Configuraciones Iniciales
    Agrecamos carpetas CSS y eliminamos el Hook

--- libro-reclamaciones.php
+++ libro-reclamaciones.php
@@ -5,10 +5,8 @@
      * Plugin URI: https://www.markethinkers.pe/plugins/libro-de-reclamaciones-peru
      * Description: Plugin creado con los estandares que debe contar un libro de reclamaciones en Perú, validado con todos los requerimientos de Indecopi. Este plugin también puede ser usado y optimizado para distintos paises del mundo.
      * Version: 1.0
-     * Author: Markethinkers
+     * Author: Jenner Acosta - Markethinkers.pe
      * Author URI: https://www.markethinkers.pe
 */
-
-register_activation_hook(__FILE__, 'Libro_Reclamaciones_Peru_init');
 
 ?>
